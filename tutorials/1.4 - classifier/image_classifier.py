c14fecb04bba9387eab0cf728e95a6cff528f44c
zzwhoops-home <zd@5rock.org>
2025-01-21 21:40:11 -0500

    move code to main()

--- tutorials/1.4 - classifier/image_classifier.py
+++ tutorials/1.4 - classifier/image_classifier.py
@@ -37,15 +37,47 @@ def imshow(img):
 
 
 def main():
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = next(dataiter)
+    # # get some random training images
+    # dataiter = iter(trainloader)
+    # images, labels = next(dataiter)
 
-    # show images
-    imshow(torchvision.utils.make_grid(images))
+    # # show images
+    # imshow(torchvision.utils.make_grid(images))
 
-    # print labels
-    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+    # # print labels
+    # print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    PATH = './cifar_net.pth'
+    torch.save(net.state_dict(), PATH)
 
 class Net(nn.Module):
     def __init__(self):
@@ -65,38 +97,6 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
-net = Net()
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-for epoch in range(2):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-
-print('Finished Training')
-
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
 
 if __name__ == '__main__':
     main()
