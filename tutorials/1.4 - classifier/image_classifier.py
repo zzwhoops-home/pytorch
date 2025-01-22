3ee2258999075e2ad1aac5a04c4e71123d4404f7
zzwhoops-home <zd@5rock.org>
2025-01-21 21:53:20 -0500

    add accuracy metrics, sending to GPU

--- tutorials/1.4 - classifier/image_classifier.py
+++ tutorials/1.4 - classifier/image_classifier.py
@@ -37,47 +37,122 @@ def imshow(img):
 
 
 def main():
-    # # get some random training images
-    # dataiter = iter(trainloader)
-    # images, labels = next(dataiter)
+    # change this to make test grid
+    grid = False
+    if (grid):
+        # get some random training images
+        dataiter = iter(trainloader)
+        images, labels = next(dataiter)
 
-    # # show images
-    # imshow(torchvision.utils.make_grid(images))
+        # show images
+        imshow(torchvision.utils.make_grid(images))
 
-    # # print labels
-    # print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+        # print labels
+        print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
 
-    net = Net()
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    for epoch in range(2):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
-
-    print('Finished Training')
-
+    # change this to enable training
+    training = False
+    test = False
+    accuracy = True
     PATH = './cifar_net.pth'
-    torch.save(net.state_dict(), PATH)
+
+    if (training):
+        net = Net()
+
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        # Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+        print(device)
+
+        net.to(device)
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+        for epoch in range(2):  # loop over the dataset multiple times
+
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+
+        print('Finished Training')
+
+        torch.save(net.state_dict(), PATH)
+    if (test):
+        dataiter = iter(testloader)
+        images, labels = next(dataiter)
+
+        # print images
+        imshow(torchvision.utils.make_grid(images))
+        print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+        net = Net()
+        net.load_state_dict(torch.load(PATH, weights_only=True))
+
+        outputs = net(images)
+
+        _, predicted = torch.max(outputs, 1)
+
+        print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
+                                    for j in range(4)))
+    if (accuracy):
+        net = Net()
+        net.load_state_dict(torch.load(PATH, weights_only=True))
+
+        # see accuracy of entire dataset
+        correct = 0
+        total = 0
+        # since we're not training, we don't need to calculate the gradients for our outputs
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                # calculate outputs by running images through the network
+                outputs = net(images)
+                # the class with the highest energy is what we choose as prediction
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+        # prepare to count predictions for each class
+        correct_pred = {classname: 0 for classname in classes}
+        total_pred = {classname: 0 for classname in classes}
+
+        # again no gradients needed
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = net(images)
+                _, predictions = torch.max(outputs, 1)
+                # collect the correct predictions for each class
+                for label, prediction in zip(labels, predictions):
+                    if label == prediction:
+                        correct_pred[classes[label]] += 1
+                    total_pred[classes[label]] += 1
+
+        # print accuracy for each class
+        for classname, correct_count in correct_pred.items():
+            accuracy = 100 * float(correct_count) / total_pred[classname]
+            print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+        
 
 class Net(nn.Module):
     def __init__(self):
